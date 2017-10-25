e0805f706e83a613dd635ea732eecaacb3207471
Guest User <Guest@Epicodus-4A.local>
2017-10-25 09:36:30 -0700

    update database to allow for each user to have saved data

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -16,6 +16,7 @@ public class Restaurant {
     double latitude;
     double longitude;
     List<String> categories = new ArrayList<>();
+    private String pushId;
 
     public Restaurant() {}
 
@@ -70,8 +71,12 @@ public class Restaurant {
         return categories;
     }
 
-//    public String getLargeImageUrl(String imageUrl) {
-//        String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 6).concat("o.jpg");
-//        return largeImageUrl;
-//    }
+    public String getPushId(){
+        return pushId;
+    }
+
+    public void setPushId(String pushId){
+        this.pushId = pushId;
+    }
+
 }
