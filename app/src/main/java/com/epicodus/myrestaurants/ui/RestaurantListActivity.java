fa41aa45423f356fea90e86e66dc11537e7d46cf
Guest User <Guest@Epicodus-4A.local>
2017-10-25 15:33:43 -0700

    ensure save restaurant button does not display while on saved restaurants scrren

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.MenuItemCompat;
@@ -21,6 +22,8 @@ import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.services.YelpService;
 import com.epicodus.myrestaurants.util.OnRestaurantSelectedListener;
 
+import org.parceler.Parcels;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -33,16 +36,50 @@ import okhttp3.Response;
 public class RestaurantListActivity extends AppCompatActivity implements OnRestaurantSelectedListener {
     private Integer mPosition;
     ArrayList<Restaurant> mRestaurants;
+    String mSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        if (savedInstanceState != null) {
+
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                mPosition = savedInstanceState.getInt(Constants.EXTRA_KEY_POSITION);
+                mRestaurants = Parcels.unwrap(savedInstanceState.getParcelable(Constants.EXTRA_KEY_RESTAURANTS));
+                mSource = savedInstanceState.getString(Constants.KEY_SOURCE);
+
+                if (mPosition != null && mRestaurants != null) {
+                    Intent intent = new Intent(this, RestaurantDetailActivity.class);
+                    intent.putExtra(Constants.EXTRA_KEY_POSITION, mPosition);
+                    intent.putExtra(Constants.EXTRA_KEY_RESTAURANTS, Parcels.wrap(mRestaurants));
+                    intent.putExtra(Constants.KEY_SOURCE, mSource);
+                    startActivity(intent);
+                }
+
+            }
+
+        }
+
     }
 
     @Override
-    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants) {
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mPosition != null && mRestaurants != null) {
+            outState.putInt(Constants.EXTRA_KEY_POSITION, mPosition);
+            outState.putParcelable(Constants.EXTRA_KEY_RESTAURANTS, Parcels.wrap(mRestaurants));
+            outState.putString(Constants.KEY_SOURCE, mSource);
+        }
+
+    }
+
+    @Override
+    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants, String source) {
         mPosition = position;
         mRestaurants = restaurants;
+        mSource = source;
     }
 }
